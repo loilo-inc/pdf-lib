216e47c79a2a0d2535a4edf9094dc468021c123e
Yusuke Sakurai <kerokerokerop@gmail.com>
2026-03-11 15:18:11 +0900

    a

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,8 @@
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
-  resolve: {
-    alias: {
-      src: resolve(__dirname, 'src'),
-    },
-  },
   test: {
     environment: 'node',
     include: ['tests/**/*.spec.ts'],
-    fileParallelism: false,
-    coverage: {
-      reporter: ['html'],
-    },
   },
 });
